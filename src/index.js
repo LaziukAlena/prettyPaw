f99c82b8a76a591558d7a3864f21b0ec5e36b564
LaziukAlena <Lazyuk.elena1988@gmail.com>
2025-12-01 15:16:00 +0300

    career footer location

--- src/index.js
+++ src/index.js
@@ -2,7 +2,38 @@ import "./index.html";
 import "normalize.css";
 import "swiper/css/bundle";
 import "./index.scss";
+import { slidersInit } from "./modules/sliders";
 
+slidersInit(".about__slider", {
+  pagination: {
+    el: ".about__slider-pagination",
+  },
+});
+
+slidersInit(".career__slider", {
+  pagination: {
+    el: ".career__slider-pagination",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: "auto",
+      spaceBetween: 20,
+      pagination: false,
+    },
+
+    1024: {
+      slidesPerView: "auto",
+      spaceBetween: 26,
+      pagination: false,
+    },
+
+    1240: {
+      slidesPerView: "auto",
+      spaceBetween: 30,
+      pagination: false,
+    },
+  },
+});
 const videoBG = document.querySelector(".video-bg");
 
 videoBG.innerHTML = ` 
